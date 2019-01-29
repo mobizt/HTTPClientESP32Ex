3247ad530509ba8aac2ceb4ff7a3afddd1e1c302
mobizt <k_suwatchai@hotmail.com>
2019-01-29 09:41:51 +0700

    Update HTTPClientESP32Ex.h

--- HTTPClientESP32Ex.h
+++ HTTPClientESP32Ex.h
@@ -45,44 +45,44 @@ class HTTPClientESP32Ex:public HTTPClient
     ~HTTPClientESP32Ex();
     
     /**
-	* Initialization of new http connection.
-	* \param host - Host name without protocols.
-	* \param port - Server's port.
-	* \param uri - The URI of resource.
-	* \param CAcert - The Base64 encode root certificate string
-	* \return True as default.
-	* If no certificate string provided, use (const char*)NULL to CAcert param 
-	*/
-	bool http_begin(const char* host, uint16_t port, const char* uri, const char* CAcert);
+    * Initialization of new http connection.
+    * \param host - Host name without protocols.
+    * \param port - Server's port.
+    * \param uri - The URI of resource.
+    * \param CAcert - The Base64 encode root certificate string
+    * \return True as default.
+    * If no certificate string provided, use (const char*)NULL to CAcert param 
+    */
+    bool http_begin(const char* host, uint16_t port, const char* uri, const char* CAcert);
 
-	/**
-	* Check the http connection status.
-	* \return True if connected.
-	*/
-	bool http_connected();
+    /**
+    * Check the http connection status.
+    * \return True if connected.
+    */
+    bool http_connected();
 
-	/**
-	* Establish http connection if header provided and send it, send payload if provided.
-	* \param header - The header string (constant chars array).
-	* \param payload - The payload string (constant chars array), optional.
-	* \return http status code, Return zero if new http connection and header and/or payload sent 
-	* with no error or no header and payload provided. If obly payload provided, no new http connection was established.
-	*/
-	int http_sendRequest(const char* header, const char* payload);
+    /**
+    * Establish http connection if header provided and send it, send payload if provided.
+    * \param header - The header string (constant chars array).
+    * \param payload - The payload string (constant chars array), optional.
+    * \return http status code, Return zero if new http connection and header and/or payload sent 
+    * with no error or no header and payload provided. If obly payload provided, no new http connection was established.
+    */
+    int http_sendRequest(const char* header, const char* payload);
 
-	/**
-	* Send extra header without making new http connection (if http_sendRequest has been called)
-	* \param header - The header string (constant chars array).
-	* \return True if header sending success.
-	* Need to call http_sendRequest with header first. 
-	*/
-	bool http_sendHeader(const char* header);
+    /**
+    * Send extra header without making new http connection (if http_sendRequest has been called)
+    * \param header - The header string (constant chars array).
+    * \return True if header sending success.
+    * Need to call http_sendRequest with header first. 
+    */
+    bool http_sendHeader(const char* header);
 
-	/**
-	* Get the WiFi client pointer.
-	* \return WiFi client pointer.
-	*/
-	WiFiClient* http_getStreamPtr(void);
+    /**
+    * Get the WiFi client pointer.
+    * \return WiFi client pointer.
+    */
+    WiFiClient* http_getStreamPtr(void);
 	
     uint16_t tcpTimeout = HTTPCLIENT_DEFAULT_TCP_TIMEOUT;
 
