1a81fb4a07e2c8b27dfcffacb57f6e6bf9e4ce5a
mobizt <k_suwatchai@hotmail.com>
2019-01-29 09:39:34 +0700

    Update HTTPClientESP32Ex.h

--- HTTPClientESP32Ex.h
+++ HTTPClientESP32Ex.h
@@ -1,9 +1,14 @@
 /*
- * Custom version of ESP32 HTTPClient Library to send custom header and payload.
+ * Customized version of ESP32 HTTPClient Library. To allow custom header and payload string sending
+ * for http and https connection.
+ *
+ * Use as part of my Firebase-ESP32 and LineNotify-ESP32 libraries for Arduino.
  * 
  * The MIT License (MIT)
  * Copyright (c) 2019 K. Suwatchai (Mobizt)
  * 
+ * HTTPClient Arduino library for ESP32
+ *
  * Copyright (c) 2015 Markus Sattler. All rights reserved.
  * This file is part of the HTTPClient for Arduino.
  * Port to ESP32 by Evandro Luis Copercini (2017), 
@@ -39,15 +44,50 @@ class HTTPClientESP32Ex:public HTTPClient
     HTTPClientESP32Ex();
     ~HTTPClientESP32Ex();
     
-    bool http_begin(const char* host, uint16_t port, const char* uri, const char* CAcert);
-    bool http_connected(void);
-    int http_sendRequest(const char* header, const char* payload);
-    WiFiClient* http_getStreamPtr(void);
+    /**
+	* Initialization of new http connection.
+	* \param host - Host name without protocols.
+	* \param port - Server's port.
+	* \param uri - The URI of resource.
+	* \param CAcert - The Base64 encode root certificate string
+	* \return True as default.
+	* If no certificate string provided, use (const char*)NULL to CAcert param 
+	*/
+	bool http_begin(const char* host, uint16_t port, const char* uri, const char* CAcert);
+
+	/**
+	* Check the http connection status.
+	* \return True if connected.
+	*/
+	bool http_connected();
+
+	/**
+	* Establish http connection if header provided and send it, send payload if provided.
+	* \param header - The header string (constant chars array).
+	* \param payload - The payload string (constant chars array), optional.
+	* \return http status code, Return zero if new http connection and header and/or payload sent 
+	* with no error or no header and payload provided. If obly payload provided, no new http connection was established.
+	*/
+	int http_sendRequest(const char* header, const char* payload);
+
+	/**
+	* Send extra header without making new http connection (if http_sendRequest has been called)
+	* \param header - The header string (constant chars array).
+	* \return True if header sending success.
+	* Need to call http_sendRequest with header first. 
+	*/
+	bool http_sendHeader(const char* header);
+
+	/**
+	* Get the WiFi client pointer.
+	* \return WiFi client pointer.
+	*/
+	WiFiClient* http_getStreamPtr(void);
+	
     uint16_t tcpTimeout = HTTPCLIENT_DEFAULT_TCP_TIMEOUT;
 
   protected:
     bool http_connect(void);
-    bool http_sendHeader(const char* header);
     TransportTraitsPtr http_transportTraits;
     std::unique_ptr<WiFiClient> _tcp;
 
